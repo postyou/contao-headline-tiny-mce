153c8e2c1f162c64f1c803e0a3b09e59f59060e5
MarkejN <markus.nestmann@outlook.com>
2022-10-18 13:34:41 +0200

    Move to cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+$header = <<<'EOF'
+    This file is part of postyou/headline-tiny-mce.
+
+    (c) POSTYOU Digital- & Filmagentur
+
+    @license LGPL-3.0+
+    EOF;
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('templates')
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/contao',
+    ])
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PHP81Migration' => true,
+        '@PHP80Migration:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'header_comment' => ['header' => $header],
+    ])
+    ->setFinder($finder)
+;
